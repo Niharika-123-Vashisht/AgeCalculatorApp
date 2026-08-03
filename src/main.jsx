012416ab809dc8c889c8fc0f9b8c96bacd6c26f0
Niharika-123-Vashisht <niharika6164104@gmail.com>
2026-08-03 10:01:35 +0530

    Fix AuthContext import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App.jsx";
-import {AuthProvider} from "./context/AuthContext";
+import {AuthProvider} from "./Context/AuthContext.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
